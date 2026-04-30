4ed2193d5a3d4a3b87663e5cde1cdd2b14d7d8e4
bbarbaraggomes <bbarbara.ggomes@gmail.com>
2026-05-01 01:22:36 +0200

    feat: GitHub Actions para build Android APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.duetto.app',
+  appName: 'Duetto',
+  webDir: 'dist'
+};
+
+export default config;
