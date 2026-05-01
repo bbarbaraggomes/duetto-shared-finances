468b7cc336fac3065cfb8ef4d75697462748a071
bbarbaraggomes <bbarbara.ggomes@gmail.com>
2026-05-01 15:50:01 +0200

    fix: raiz redireciona para landing.html

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
 import LoginPage from "./LoginPage";
 
-const Index = () => <LoginPage />;
+const Index = () => {
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      window.location.replace("/landing.html");
+    }
+  }, []);
+
+  if (window.location.pathname === "/") return null;
+
+  return <LoginPage />;
+};
 
 export default Index;
